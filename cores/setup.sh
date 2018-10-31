a8cb65960d91de0b3d67bfa009affba7130a8b1d
Christopher Rokita <chris@livio.io>
2018-10-31 13:32:23 -0400

    Include new module types and make transport manager less verbose

--- cores/setup.sh
+++ cores/setup.sh
@@ -42,7 +42,7 @@ echo "Running core version ${CORE_VERSION}"
 perl -pi -e 's/127.0.0.1/'$DOCKER_IP'/g' /usr/build/bin/smartDeviceLink.ini
 
 # Set the default preloaded policy table to allow access to everything....... everything
-perl -pi -e 's/\[\"Base-4\"\]/\[\"Base-4\", \"Location-1\", \"Notifications\", \"DrivingCharacteristics-3\", \"VehicleInfo-3\", \"PropriataryData-1\", \"PropriataryData-2\", \"ProprietaryData-3\", \"Emergency-1\", \"Navigation-1\", \"Base-6\", \"OnKeyboardInputOnlyGroup\", \"OnTouchEventOnlyGroup\", \"DiagnosticMessageOnly\", \"DataConsent-2\", \"BaseBeforeDataConsent\", \"SendLocation\", \"WayPoints\", \"BackgroundAPT\", \"Notifications-RC\", \"RemoteControl\", \"HapticGroup\"\],\n\"moduleType\"\: \[\"CLIMATE\", \"RADIO\"\] /g' /usr/build/bin/sdl_preloaded_pt.json
+perl -pi -e 's/\[\"Base-4\"\]/\[\"Base-4\", \"Location-1\", \"Notifications\", \"DrivingCharacteristics-3\", \"VehicleInfo-3\", \"PropriataryData-1\", \"PropriataryData-2\", \"ProprietaryData-3\", \"Emergency-1\", \"Navigation-1\", \"Base-6\", \"OnKeyboardInputOnlyGroup\", \"OnTouchEventOnlyGroup\", \"DiagnosticMessageOnly\", \"DataConsent-2\", \"BaseBeforeDataConsent\", \"SendLocation\", \"WayPoints\", \"BackgroundAPT\", \"Notifications-RC\", \"RemoteControl\", \"HapticGroup\"\],\n\"moduleType\"\: \[\"CLIMATE\", \"RADIO\", \"SEAT\", \"AUDIO\", \"LIGHT\", \"HMI_SETTINGS\"\] /g' /usr/build/bin/sdl_preloaded_pt.json
 
 # Add the ability to send logs to stdout. Do not write logs to file
 perl -pi -e 's/ALL, SmartDeviceLinkCoreLogFile/ALL, SmartDeviceLinkCoreLogFile/g' /usr/build/bin/log4cxx.properties
