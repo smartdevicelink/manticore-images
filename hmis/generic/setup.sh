0b79feda373b0f67db7a15ab3d4584575c83275e
Christopher Rokita <chris@livio.io>
2019-07-10 14:30:44 -0400

    Remove requirement of specifying echo-protocol in websocket connection

--- hmis/generic/setup.sh
+++ hmis/generic/setup.sh
@@ -35,9 +35,6 @@ echo "Changing Nginx file proxy address to ${CORE_FILE_ADDR}"
 # Replace IP and Port in Controller file with the address to the broker
 # The address for the broker is REQUIRED to include the protocol (ex. ws://localhost:80)
 perl -pi -e "s/ws:\/\/localhost:8087/$BROKER_ADDR/g" /usr/app/webapp/build/bundle.js
-# The HMI doesn't specify a subprotocol! We need it to specify echo-protocol when requesting
-# a connection, otherwise the sdl_broker will deny the connection!
-perl -pi -e "s/WebSocket\(url\)/WebSocket\(url, \['echo-protocol'\]\)/g" /usr/app/webapp/build/bundle.js
 # Replace XXXXX in the nginx conf file with the address of sdl_core
 perl -pi -e "s/XXXXX/$CORE_FILE_ADDR/g" /etc/nginx/nginx.conf
 # Start nginx
